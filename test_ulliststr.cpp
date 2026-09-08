26d546d9aa501542e69c8259c915ad791efaad64
Max Allison <maxallis@usc.edu>
2026-09-07 20:53:27 -0700

    Finished section 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,35 @@
 
 int main(int argc, char* argv[])
 {
+    ULListStr dat;
+    dat.push_back("7");
+    dat.push_front("8");
+    dat.push_back("9");
+    std::cout << dat.get(0) << " " << dat.get(1) << " " << dat.get(2) << std::endl;
+    std::cout << dat.size() << std::endl; 
+    dat.pop_front();
+    std::cout << dat.get(0) << std::endl;
+    dat.push_front("19");
+    dat.pop_back();
+    std::cout << dat.get(1) << std::endl;
+    dat.pop_back();
+    dat.pop_back();
+    dat.pop_back();
+    dat.pop_back();
+    std::cout << dat.size() << std::endl;
+    dat.push_front("2");
+    dat.push_back("11");
+    std::cout << dat.front() << std::endl;
+    std::cout << dat.back() << std::endl;
+    dat.clear();
+    for (int i = 0; i < 30; i++) {
+        dat.push_front(std::to_string(i));
+    }
+    std::cout << dat.get(0) << " " << dat.get(23) << " " << dat.get(35) << std::endl;
+
+
+
+
+
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -63,3 +63,116 @@ void ULListStr::clear()
   tail_ = NULL;
   size_ = 0;
 }
+
+void ULListStr::push_back(const std::string& val) {
+  if (tail_ != NULL) {
+    if (tail_->last == ARRSIZE) {
+      tail_->next = new Item();
+      tail_->next->prev = tail_;
+      tail_ = tail_->next;
+    }
+  } else {
+    tail_ = new Item();
+    head_ = tail_;
+  }
+  tail_->val[tail_->last] = val;
+  tail_->last++;
+  size_++;
+}
+
+void ULListStr::pop_back() {
+  if (!empty()) {
+    if (tail_->last - tail_->first == 1) {
+      if (tail_ != head_) {
+        Item* temp = tail_;
+        tail_ = tail_->prev;
+        tail_->next = NULL;
+        delete temp;
+        size_--;
+      }
+      else {
+        delete tail_;
+        head_ = NULL;
+        tail_ = NULL;
+        size_--;
+      }
+    } else {
+      tail_->last--;
+      size_--;
+    }
+  }
+}
+
+void ULListStr::push_front(const std::string& val) {
+  if (head_ != NULL) {
+    if (head_->first == 0) {
+      head_->prev = new Item();
+      head_->prev->next = head_;
+      head_ = head_->prev;
+      head_->first = ARRSIZE;
+      head_->last = ARRSIZE;
+    }
+  } else {
+    head_ = new Item();
+    tail_ = head_;
+    head_->first = ARRSIZE;
+    head_->last = ARRSIZE;
+  }
+  head_->first--;
+  head_->val[head_->first] = val;
+  size_++;
+}
+
+void ULListStr::pop_front() {
+  if (!empty()) {
+    if (head_->last - head_->first == 1) {
+      if (head_ != tail_) {
+        Item* temp = head_;
+        head_ = head_->next;
+        head_->prev = NULL;
+        delete temp;
+        size_--;
+      } else {
+        delete head_;
+        head_ = NULL;
+        tail_ = NULL;
+        size_--;
+      }
+    } else {
+      head_->first++;
+      size_--;
+    }
+  }
+}
+
+std::string const& ULListStr::back() const {
+  if (!empty()) {
+    return tail_->val[tail_->last-1];
+  } else {
+    throw std::out_of_range("Empty List");
+  }
+}
+
+std::string const& ULListStr::front() const {
+  if (!empty()) {
+    return head_->val[head_->first];
+  } else {
+    throw std::out_of_range("Empty List");
+  }
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  if (loc >= size_) {
+    return NULL;
+  }
+  Item* temp = head_;
+  while (temp != NULL) {
+    int count = temp->last - temp->first;
+    if (loc < count) {
+      return &temp->val[temp->first + loc];
+    } else {
+      loc -= count;
+      temp = temp->next;
+    }
+  }
+}
